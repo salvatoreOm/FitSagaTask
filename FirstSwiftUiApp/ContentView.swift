cb9f1e7ab5ce37b89532f3a6c46d50d3985592d2
OM PARIHAR <omparihar0001@gmail.com>
2024-09-11 14:30:57 +0530

    Initial Commit

---
+++ FirstSwiftUiApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirstSwiftUiApp
+//
+//  Created by Om Parihar on 11/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstSwiftUiApp/FirstSwiftUiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUiAppApp.swift
+//  FirstSwiftUiApp
+//
+//  Created by Om Parihar on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
